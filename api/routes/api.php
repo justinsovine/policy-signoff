c71ecd54d3c5f06cb028b17479e3766b3a4149a9
Justin Sovine <justinsovine@gmail.com>
2026-02-17 17:03:55 -0500

    Add presigned URL endpoints for file upload and download

--- api/routes/api.php
+++ api/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\FileController;
 use App\Http\Controllers\PolicyController;
 use App\Http\Controllers\SignoffController;
 use Illuminate\Support\Facades\Route;
@@ -9,4 +10,6 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::post('/policies', [PolicyController::class, 'store']);
     Route::get('/policies/{policy}', [PolicyController::class, 'show']);
     Route::post('/policies/{policy}/signoff', [SignoffController::class, 'store']);
+    Route::post('/policies/{policy}/upload-url', [FileController::class, 'uploadUrl']);
+    Route::get('/policies/{policy}/download-url', [FileController::class, 'downloadUrl']);
 });
