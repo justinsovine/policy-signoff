4867c38e615bb093878947e6b61cc9caffc37852
Justin Sovine <justinsovine@gmail.com>
2026-02-23 16:31:35 -0500

    Add Google Analytics (GA4) via react-ga4
    - Initialize GA4 with measurement ID in main.tsx
    - Track SPA pageviews on route changes in AppRoutes

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,8 +2,11 @@ import '@/index.css'
 
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import ReactGA from 'react-ga4'
 
 import App from '@/App'
+
+ReactGA.initialize('G-HWEJ5GYNDV')
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
